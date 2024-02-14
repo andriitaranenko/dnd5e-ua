854cd644de63d2dbeac803ea531688a9834b5a8a
Andrii Taranenko <taranenkoandrii08@gmail.com>
2024-02-14 17:11:31 +0200

    enable CORS

--- dnd-ua-api/src/main.ts
+++ dnd-ua-api/src/main.ts
@@ -10,10 +10,11 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
