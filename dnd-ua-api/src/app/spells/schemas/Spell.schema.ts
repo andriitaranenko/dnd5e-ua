b7b4145a3d8546528303829fd647fd7579160433
Andrii Taranenko <taranenkoandrii08@gmail.com>
2024-02-29 14:03:01 +0200

    update spell schema

--- dnd-ua-api/src/app/spells/schemas/Spell.schema.ts
+++ dnd-ua-api/src/app/spells/schemas/Spell.schema.ts
@@ -1,40 +1,80 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { AbilityScore, AreaType, DamageType, SaveSuccessEffect, SpellCastingTime, SpellComponents, SpellUser } from "@dnd-ua/shared-lib";
 
 @Schema()
 export class Spell {
 
-  @Prop()
+  @Prop({ required: true })
   originalName: string;
   
-  @Prop()
+  @Prop({ required: true })
   sourceUrl: string;
   
-  @Prop()  
+  @Prop({ required: true })  
   translatedName: string;
   
-  @Prop()
-  level: number;
-  
-  @Prop()
-  castingTime: string;
+  @Prop({ required: true })
+  level: number; // 0 == Cantrip, 1 - 9
 
-  @Prop()
+  @Prop({ required: true })
   concentration: boolean;
+
+  @Prop({ required: true })
+  ritual: boolean;
+
+  @Prop({ required: false })
+  effect?: string;
   
-  @Prop()
-  range: number;
+  @Prop({ type: String, enum: SpellCastingTime, required: true })
+  castingTime: SpellCastingTime;
   
-  @Prop([String])
-  components: string[];
+  @Prop({ required: true })
+  range: number; // -1 == Self, 0 == Touch, 1, ..., n
+
+  @Prop({ required: false })
+  area?: number;
+
+  @Prop({ type: String, enum: AreaType, required: false })
+  areaType?: AreaType;
   
-  @Prop()
+  @Prop({ type: [String], enum: SpellComponents, required: true })
+  components: SpellComponents[];
+
+  @Prop({ required: false })
+  materials?: string;
+  
+  @Prop({ required: true })
   duration: number;
   
-  @Prop()
+  @Prop({ required: true })
   description: string;
+
+  @Prop({ required: false, default: '' })
+  higherLevel?: string;
   
-  @Prop([String])
-  spellUsers: string[];
+  @Prop({ type: [String], enum: SpellUser, required: true })
+  spellUser: SpellUser[];
+
+  @Prop({ type: String, enum: AbilityScore, required: false })
+  saveType?: AbilityScore;
+
+  @Prop({ type: String, enum: SaveSuccessEffect, required: false })
+  saveSuccess?: SaveSuccessEffect;
+
+  @Prop({ type: String, enum: DamageType, required: false })
+  damageType?: DamageType;
+
+  @Prop({ required: false })
+  damageHP?: string; // e.g. 1d6
+
+  @Prop({ required: false })
+  damageLevelModifier?: string;
+
+  @Prop({ required: false })
+  healHP?: string;
+
+  @Prop({ required: false })
+  healLevelModifier?: string;
 }
 
 export const SpellSchema = SchemaFactory.createForClass(Spell);
---
+++ shared-lib/src/lib/models/spell.models.ts
@@ -0,0 +1,69 @@
+export enum SpellComponents {
+  VERBAL = 'verbal',
+  SEMANTIC = 'semantic',
+  MATERIAL = 'material',
+}
+
+export enum SpellCastingTime {
+  ACTION = 'action',
+  BONUS_ACTION = 'bonus_action',
+  REACTION = 'reaction',
+  ONE_MINUTE = 'one_minute',
+  TEN_MINUTES = 'ten_minutes',
+  ONE_HOUR = 'one_hour',
+  EIGHT_HOURS = 'eight_hour',
+}
+
+export enum AreaType {
+  CIRCLE = 'circle',
+  SQUARE = 'square',
+  SPHERE = 'sphere',
+  CUBE = 'cube',
+  CYLINDER = 'cylinder',
+}
+
+export enum AbilityScore {
+  CHA = 'cha',
+  CON = 'con',
+  DEX = 'dex',
+  INT = 'int',
+  WIS = 'wis',
+  STR = 'str',
+}
+
+export enum SaveSuccessEffect {
+  NO = 'no',
+  HALF = 'half',
+  FULL = 'full',
+}
+
+export enum DamageType {
+  ACID = 'acid',
+  BLUDGEONING = 'bludgeoning',
+  COLD = 'cold',
+  FIRE = 'fire',
+  FORCE = 'force',
+  LIGHTNING = 'lightning',
+  NECROTIC = 'necrotic',
+  PIERCING = 'piercing',
+  POISON = 'poison',
+  PSYCHIC = 'psychic',
+  RADIANT = 'radiant',
+  SLASHING = 'slashing',
+  THUNDER = 'thunder',
+}
+
+export enum SpellUser {
+  BARBARIAN = 'barbarian',
+  BARD = 'bard',
+  CLERIC = 'cleric',
+  DRUID = 'druid',
+  FIGHTER = 'fighter',
+  MONK = 'monk',
+  PALADIN = 'paladin',
+  RANGER = 'ranger',
+  ROGUE = 'rogue',
+  SORCERER = 'sorcerer',
+  WARLOCK = 'warlock',
+  WIZARD = 'WIwizardZARD',
+}
